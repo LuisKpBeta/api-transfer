0044be70b5ef0665b884b20d5a5d21f643e0eeef
Luis <lu.ca.oliveira41@gmail.com>
2023-09-19 22:17:44 -0400

    feature: return error on invalid transfer operation

--- internal/usecase/create-user-transfer.go
+++ internal/usecase/create-user-transfer.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"errors"
+	"time"
 
 	user "github.com/LuisKpBeta/api-transfer/internal/domain"
 	repository "github.com/LuisKpBeta/api-transfer/internal/infra/database/interface"
@@ -17,17 +18,36 @@ func (c *CreateUserTransfer) CreateUserTransfer(senderId, receiverId string, tot
 		return errors.New("invalid value for total:" + total)
 	}
 	newTransfer := user.Transfer{
-		SenderId:   senderId,
-		ReceiverId: receiverId,
-		Total:      value,
+		SenderId:      senderId,
+		ReceiverId:    receiverId,
+		Total:         value,
+		OperationDate: time.Now(),
 	}
-	senderBalance, err := c.UserRepository.LoadUserBalanceById(newTransfer.SenderId)
+	sender, err := c.UserRepository.LoadUserById(newTransfer.SenderId)
 	if err != nil {
 		return err
 	}
-	if senderBalance < value {
+	if sender.Balance < value {
 		return user.ErrSenderBalanceInvalid
 	}
+	receiver, err := c.UserRepository.LoadUserById(receiverId)
+	if err != nil {
+		return err
+	}
+	receiver.Balance += newTransfer.Total
+	sender.Balance -= newTransfer.Total
 
+	err = c.UserRepository.UpdateUserBalanceById(receiver)
+	if err != nil {
+		return err
+	}
+	err = c.UserRepository.UpdateUserBalanceById(sender)
+	if err != nil {
+		return err
+	}
+	err = c.UserRepository.CreateUserTransfer(newTransfer)
+	if err != nil {
+		return err
+	}
 	return nil
 }
