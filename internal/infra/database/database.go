960377274ea22a68f3a834d35557807238a1aa32
Luis <lu.ca.oliveira41@gmail.com>
2023-09-21 21:25:40 -0400

    feature: setup transactions for transfer operations

--- internal/infra/database/database.go
+++ internal/infra/database/database.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"log"
+	"time"
 
 	user "github.com/LuisKpBeta/api-transfer/internal/domain"
 	"github.com/google/uuid"
@@ -35,16 +38,46 @@ func (u *UserRepository) LoadUserById(id string) (*user.User, error) {
 	}
 	return &user, nil
 }
-func (u *UserRepository) UpdateUserBalanceById(user *user.User) error {
-	_, err := u.Db.Exec("UPDATE client SET balance=$1 WHERE id = $2", user.Balance, user.Id)
+func (u *UserRepository) StartTransaction() *sql.Tx {
+	tx, _ := u.Db.Begin()
+	return tx
+}
+func (u *UserRepository) AbortTransaction(tx *sql.Tx) {
+	log.Println("aborting transaction")
+	err := tx.Rollback()
+	log.Println(err.Error())
+}
+func (u *UserRepository) EndTransaction(tx *sql.Tx) error {
+	log.Println("transaction successfull")
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
+func (u *UserRepository) TrxLoadUserById(tx *sql.Tx, id string) (*user.User, error) {
+	var user user.User
+	err := tx.QueryRow("SELECT id, name, balance FROM client WHERE id = $1", id).
+		Scan(&user.Id, &user.Name, &user.Balance)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+func (u *UserRepository) UpdateUserBalanceById(tx *sql.Tx, userId string, value int) error {
+	_, err := tx.Exec("UPDATE client SET balance = balance + $1 WHERE id = $2", value, userId)
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func (u *UserRepository) CreateUserTransfer(transfer user.Transfer) error {
+func (u *UserRepository) CreateUserTransfer(tx *sql.Tx, transfer user.Transfer) error {
 	transfer.Id = uuid.NewString()
-	_, err := u.Db.Exec("INSERT INTO transfers (id, sender_id, receiver_id, total, operation_date) VALUES ($1, $2, $3, $4, $5)",
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	_, err := tx.ExecContext(ctx, "INSERT INTO transfers (id, sender_id, receiver_id, total, operation_date) VALUES ($1, $2, $3, $4, $5)",
 		transfer.Id, transfer.SenderId, transfer.ReceiverId, transfer.Total, transfer.OperationDate)
 	if err != nil {
 		return err
--- internal/infra/database/interface/user-repository.go
+++ internal/infra/database/interface/user-repository.go
@@ -1,9 +1,17 @@
 package repository
 
-import user "github.com/LuisKpBeta/api-transfer/internal/domain"
+import (
+	"database/sql"
+
+	user "github.com/LuisKpBeta/api-transfer/internal/domain"
+)
 
 type UserRepostory interface {
 	LoadUserById(id string) (*user.User, error)
-	UpdateUserBalanceById(user *user.User) error
-	CreateUserTransfer(transfer user.Transfer) error
+	UpdateUserBalanceById(tx *sql.Tx, userId string, value int) error
+	CreateUserTransfer(tx *sql.Tx, transfer user.Transfer) error
+	TrxLoadUserById(tx *sql.Tx, id string) (*user.User, error)
+	StartTransaction() *sql.Tx
+	EndTransaction(tx *sql.Tx) error
+	AbortTransaction(tx *sql.Tx)
 }
